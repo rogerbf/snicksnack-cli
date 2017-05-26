fa847a065b59ed125b386161cd82d2df55d4ee00
rogerbf <roger@bergfalk.nu>
2017-05-26 13:47:35 +0200

    words, sentences, clipboard

--- source/binary/cli.js
+++ source/binary/cli.js
@@ -1,6 +1,31 @@
 #!/usr/bin/env node
 const snicksnack = require(`snicksnack`)
-const commander = require(`commander`)
-const clipboard = require(`clipboard`)
+const application = require(`commander`)
+const clipboard = require(`clipboardy`)
 
+const output = data =>
+  application.clipboard
+  ? clipboard.writeSync(data)
+  : console.log(data)
 
+application
+.version(`1.0.0`)
+.option(`-c, --clipboard`, `output to clipboard`)
+
+application
+.command(`words <number>`)
+.alias(`w`)
+.description(`output <number> of words`)
+.action((number, options) => {
+  output(snicksnack.words(parseInt(number, 10)).join(` `))
+})
+
+application
+.command(`sentences <number>`)
+.alias(`s`)
+.description(`output <number> of sentences`)
+.action((number, options) => {
+  output(snicksnack.sentences(parseInt(number, 10)).join(` `).trim())
+})
+
+application.parse(process.argv)
